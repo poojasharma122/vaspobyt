823eecf799ee1544f51972fca931fd4aaeded253
komalverma06 <komalverma1906@gmail.com>
2025-04-25 17:33:06 +0530

    change

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,3 +23,20 @@ AOS.init({
   duration:1200,
 });
 // AOS JS End
+
+
+
+  // Read More Functionality JS Start
+  $('.subpage-readmore-btn').click(function (e) {
+    e.preventDefault();
+    const $this = $(this);
+    const content = $this.closest('.hotels_card_content').find('.hidden_content');
+    content.stop(true, true).slideToggle(500, function () {
+        const isVisible = content.is(':visible');   
+        $this.html(isVisible
+            ? 'Číst méně <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M201.4 137.4c12.5-12.5 32.8-12.5 45.3 0l160 160c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L224 205.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l160-160z"/></svg>'
+            : 'Přečtěte si více <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M201.4 374.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 306.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>');
+    });
+});
+
+  // Read More Functionality JS end
